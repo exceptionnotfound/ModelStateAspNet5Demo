cadb3afe79114930099c9bf5bd881597c94a7457
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2021-02-12 11:33:16 -0700

    Add project files.

---
+++ ModelStateCoreDemo/Controllers/MovieController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelStateCoreDemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelStateCoreDemo.Controllers
+{
+    public class MovieController : Controller
+    {
+        [HttpGet("~/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("movies/add")]
+        public IActionResult Add()
+        {
+            AddMovieVM model = new AddMovieVM();
+            return View(model);
+        }
+
+        [HttpPost("movies/add/post")]
+        public IActionResult AddPost(AddMovieVM model)
+        {
+            if(model.Title == model.Description)
+            {
+                ModelState.AddModelError("description", "The Description cannot exactly match the Title.");
+            }
+
+            if(!ModelState.IsValid)
+            {
+                return View("Add", model);
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ModelStateCoreDemo/ViewModels/AddMovieVM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModelStateCoreDemo.ViewModels
+{
+    public class AddMovieVM
+    {
+        [Required(ErrorMessage = "The Title cannot be blank.")]
+        [DisplayName("Title: ")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "The Description cannot be blank.")]
+        [DisplayName("Description: ")]
+        public string Description { get; set; }
+
+        [DisplayName("Release Date: ")]
+        public DateTime ReleaseDate { get; set; }
+
+        [Required(ErrorMessage = "The Runtime Minutes cannot be blank.")]
+        [Range(1, int.MaxValue, ErrorMessage = "The Runtime Minutes must be greater than 0.")]
+        [DisplayName("Runtime Minutes: ")]
+        public int RuntimeMinutes { get; set; }
+    }
+}
